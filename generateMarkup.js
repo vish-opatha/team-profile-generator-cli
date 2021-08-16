54abdefd3f14baf7cf20267ecaa6294c9baeabd7
Vish O <v.opatha@gmail.com>
2021-08-16 21:26:27 +0930

    Update index.js

--- generateMarkup.js
+++ generateMarkup.js
@@ -1,69 +1,49 @@
-let ma
-function generateMarkdown(teamDataArray) {
-    licenseBadge = renderLicenseBadge(data.proLicence);
-    licenseSection = renderLicenseSection(data.proLicence);
-  
-    var markDown = 
-    `# ${data.proName}
-    ${licenseBadge}
-       
-    ## Description
-    ${data.proDecription}
-    
-    ## Table of Contents
-    * [Installation](#Installation)
-    * [Usage](#Usage)
-    * [License](#License)
-    * [Contribute](#Contributing)
-    * [Tests](#Tests)
-    * [Questions](#Questions)
-    
-    ## Installation
-    To install the necessary dependencies, run the following command:
-    \`\`\`
-    ${data.proDependencies} 
-    \`\`\`
-    
-    ## Usage
-    ${data.proRepo}
-    ## License
-    ${licenseSection}
-    ## Contributing
-    ${data.proContribution}
-    ## Tests
-    To run the tests, run the following command;
-    \`\`\`
-    ${data.proTest}
-    \`\`\`
-    ## Questions
-    If you have any questions about the repo, open an issue or directly contact me at <${data.uEmail}> You can find my other work at [${data.uName}](https://github.com/${data.uName})`;
-  
-    return markDown;
-  }
+let markUpText = "";
+let remainder = 0;
+let numOfSections = 0;
+let remainingItems = 0;
 
+// Function to decide the number of sections and columns needed for the layout
+// I am using Bulma and sample index file can be found on the repository as index.html
+function countColumns(teamDataArray){
+    let arrLength = teamDataArray.arrLength;
 
+    if(remainder===0) {
+      numOfSections = arrLength / 3;
+    }
 
+    else{
+      remainingItems = arrLength - remainder;
+      numOfSections = (items/3) + 1 ;
+    }
+}
 
+// This function generates the markup text to write to the file.
+function generateMarkupText(teamArray){
+    countColumns(teamArray);
+    markUpText = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+        <title>Software Development Team</title>
+      </head>
 
+      <body>
+        <!--Header section with the topic-->
+        <div class="section has-background-success-dark has-shadow is-vcentered">
+            <div class="container ">
+                <h1 class="is-size-3 title has-text-centered has-text-light "> Software Development Team </h1>
+            </div>
+        </div> `;
+        //The part of the function is yet to be completed.
+        //Please see the designed index.html file.
 
+    return markUpText;
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = generateMarkup;
+module.exports = generateMarkupText;
--- index.js
+++ index.js
@@ -1,125 +1,156 @@
 // Linking required packages and classes
 const inquirer = require("inquirer");
-// const chalkPipe = require('chalk-pipe');
 const Manager = require("./lib/Manager");
 const Engineer = require ("./lib/Engineer");
-const intern = require("./lib/Intern");
+const Intern = require("./lib/Intern");
+const generateMarkupText = require('./generateMarkup.js');
 let team = [];
 
-const questions = [
-    {
-      type: 'input',
-      name: 'manager',
-      message: "What's the team manager's name?",
-    },
-    {
-      type: 'input',
-      name: 'mgrId',
-      message: "What's the team manager's Id?",
-    //   default() {
-    //     return 'Doe';
-    //   },
-    },
-    {
-      type: 'input',
-      name: 'mgrEmail',
-      message: "What's the team manager's email?",
-    //   transformer(color, answers, flags) {
-    //     const text = chalkPipe(color)(color);
-    //     if (flags.isFinal) {
-    //       return text + '!';
-    //     }
-  
-    //     return text;
-    //   },
-    },
-    {
-      type: 'input',
-      name: 'phone',
-      message: "What's your phone number",
-      validate(value) {
-        const pass = value.match(
-          /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
-        );
+// Questions to create Manager object
+const mgrQuestions = [
+    { type: 'input', name: 'mgrName', message: "What's the team manager's name?",
+        validate(value) {
+        const pass = value.match(/^[a-zA-Z]+$/);
         if (pass) {
           return true;
         }
-  
-        return 'Please enter a valid phone number! Avoid using spces or hyphens in between.';
+        return `Please enter the manager's name`;
+       },
+    },
+    { type: 'input', name: 'mgrId', message: "What's the team manager's Id?", },
+    { type: 'input', name: 'mgrEmail', message: "What's the team manager's email?",
+        validate(value) {
+          const pass = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+          if (pass) {
+            return true;
+          }
+          return 'Please enter a valid email address';
+        },
+      },
+    { type: 'input', name: 'phone', message: "What's your phone number?",
+        validate(value) {
+          const pass = value.match(
+            /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
+          );
+          if (pass) {
+            return true;
+          }
+          return 'Please enter a valid phone number! Enter 10 numbers.';
+        },
+      },
+    ];
+
+// Question to select the type of team member /^[a-z\u00C0-\u02AB'´`]+\.?\s([a-z\u00C0-\u02AB'´`]+\.?\s?)+$/
+const questionsTeam = [
+    { type: 'list', name: 'typeOfTeamMember', message: "What type of team member would you like to add?",
+      choices: [`Engineer`, `Intern`, `I don't want to add anyone`],},];
+
+// Questions to create an Engineer object
+const engineerQuestions = [
+    { type: 'input', name: 'eName', message: "What's your engineer's name?", 
+      validate(value) {
+        const pass = value.match(/^[a-zA-Z]+$/);
+        if (pass) {
+          return true;
+        }
+        return `Please enter the engineer's name`;
       },
     },
-  ];
-
-  const questionsTeam = [
-    {
-        type: 'list',
-        name: 'typeOfTeamMember',
-        message: "What type of team member would you like to add?",
-        choices: [`Engineer`, `Intern`, `I don't want to add anyone`],
-    },
-  ];
-
-  const engineerQuestions = [
-    { type: 'input', name: 'engineer', message: "What's your engineer's name?", },
     { type: 'input', name: 'engId', message: "What's your engineer's Id?", },
-    { type: 'input', name: 'engEmail', message: "What's your engineer's email?",},
-    { type: 'input', name: 'gitHub', message: "What's your engineer's Github?",},
-  ];
+    { type: 'input', name: 'engEmail', message: "What's your engineer's email?",
+      validate(value) {
+            const pass = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+            if (pass) {
+                return true;
+            }
+            return 'Please enter a valid email address';
+        },
+    }, 
+    { type: 'input', name: 'gitHub', message: "What's your engineer's Github?",},];
 
-  const internQuestions = [
-    { type: 'input', name: 'intern', message: "What's your intern's name?", },
+// Questions to create an Intern object   
+const internQuestions = [
+    { type: 'input', name: 'iName', message: "What's your intern's name?",
+      validate(value) {
+        const pass = value.match(/^[a-zA-Z]+$/);
+        if (pass) {
+          return true;
+        }
+        return `Please enter the intern's name`;
+      },
+    },
     { type: 'input', name: 'intId', message: "What's your intern's Id?", },
-    { type: 'input', name: 'intEmail', message: "What's your intern's email?",},
-    { type: 'input', name: 'school', message: "What's your intern's school?",},
-  ];
-
-  inquirer.prompt(questions).then((answers) => {
-      team.push(JSON.stringify(answers));
-  }).then(()=>{
-    buildTeam();
-        // if(anwersTwo.typeOfTeamMember ==='Engineer')
-        // {
-        //     inquirer.prompt(engineerQuestions).then((answers) => {
-        //         team.push(JSON.stringify(answers));
-        //     }).then(()=>{
-        //          buildTeam();
-        //     }); 
-        //     // buildTeam();  
-        // }
-    });
-  
-
-  
-
-  
-  function buildTeam(){
-    inquirer.prompt(questionsTeam).then((answers) => {
-        if(answers.typeOfTeamMember ==='Engineer')
-        {
-            inquirer.prompt(engineerQuestions).then((answers) => {
-                team.push(JSON.stringify(answers));
-                
-            }).then(()=>{
-                buildTeam();
-            }); 
-              
+    { type: 'input', name: 'intEmail', message: "What's your intern's email?",
+      validate(value) {
+        const pass = value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        if (pass) {
+            return true;
         }
 
-        else if(answers.typeOfTeamMember === 'Intern'){
+        return `Please enter a valid email address`;
+        },
+    },
+    { type: 'input', name: 'school', message: "What's your intern's school?",
+      validate(value) {
+        const pass = value.match(/^[a-zA-Z]+$/);
+        if (pass) {
+          return true;
+        }
+        return `Please enter the school.`;
+      },},
+    ];
+
+// Function to write team details to HTML
+function writeToHtml(fileName, data) {
+      const markUpText = generateMarkupText(data);
+      fs.writeFile(fileName,markUpText,(err)=>
+      err ? console.log(err) : console.log('You have successfully created your team!'));
+};
+
+
+// Function to create the other members except the manager
+function buildTeam(){
+    inquirer.prompt(questionsTeam).then((answers) => {
+        // If the selected team member is an engineer
+        if(answers.typeOfTeamMember ==='Engineer') {
+            inquirer.prompt(engineerQuestions).then((answers) => {
+                let engObj = new Engineer(answers.engId,answers.eName,answers.engEmail,answers.gitHub);
+                team.push(JSON.stringify(engObj));
+                // team.push(engObj);    
+                }).then(() => { buildTeam(); }); 
+        }
+
+        // If the selected team member is an intern
+        else if(answers.typeOfTeamMember === 'Intern') {
             inquirer.prompt(internQuestions).then((answers) => {
-                team.push(JSON.stringify(answers));
-            }).then(()=>{
-                buildTeam();
-            }); 
+                let internObj = new Intern(answers.intId, answers.iName, answers.intEmail,answers.school);
+                team.push(JSON.stringify(internObj));
+                //team.push(internObj);
+                }).then(() => { buildTeam(); }); 
         }
         
-        else{
+        // If the user selects No one to add to the team html file will be created.
+        else {
+            let filename = "index.html";
             console.log(team);
+            // Function buildTeam is supposed to be called here.
+            // writeToHtml(fileName, team)
         }
-      });
-  }
+    });
+}
 
- 
+// Initiallization function 
+function init() {
+    inquirer.prompt(mgrQuestions).then((answers) => {
+        let mgrObj = new Manager(answers.mgrId,answers.mgrName,answers.mgrEmail,answers.phone);
+        team.push(JSON.stringify(mgrObj));
+        }).then(()=>{
+            buildTeam();});      
+};
+
+// Function call to initialize the application
+init(); 
+
   
 
 
