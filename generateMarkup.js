59df891f63178b530c6bd3f241dd7733dc031dce
Vish O <v.opatha@gmail.com>
2021-11-03 08:48:21 +1030

    Update css in emploeyee divs

--- generateMarkup.js
+++ generateMarkup.js
@@ -26,13 +26,13 @@ async function generateMarkup(teamObjectArray) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Team</title>
+  <title>Software Development Team</title>
  
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 </head>
 <body>
-  <header class="container-fluid bg-primary p-3">
+  <header class="container-fluid bg-danger p-3">
       <h1 class="text-white text-center">Software Development Team</h1>
   </header>
 
@@ -56,11 +56,11 @@ async function generateMarkup(teamObjectArray) {
 function generateManagerDiv(mgr) {
   return `
   <div class="card m-3 p-0 bg-light border" style="width: 18rem;">
-      <div class="card-body bg-danger text-white">
+      <div class="card-body bg-dark text-white">
           <h5 class="card-title">${mgr.empName}</h5>
           <p class="card-text">Manager</p>
       </div>
-      <ul class="list-group m-3 border border-secondary">
+      <ul class="list-group m-3">
           <li class="list-group-item">ID: ${mgr.id}</li>
           <li class="list-group-item">Email: <a href="mailto:${mgr.empEmail}">${mgr.empEmail}</a></li>
           <li class="list-group-item">Office Number: ${mgr.officeNumber}</li>
@@ -72,11 +72,11 @@ function generateManagerDiv(mgr) {
 function generateEngineerDiv(eng) {
   return `
   <div class="card m-3 p-0 bg-light border" style="width: 18rem;">
-      <div class="card-body bg-success text-white">
+      <div class="card-body bg-secondary text-white">
           <h5 class="card-title">${eng.empName}</h5>
           <p class="card-text">Engineer</p>
       </div>
-      <ul class="list-group list-group-flush m-3 border border-secondary">
+      <ul class="list-group m-3">
           <li class="list-group-item">ID: ${eng.id}</li>
           <li class="list-group-item">Email: <a href="mailto:${eng.empEmail}">${eng.empEmail}</a></li>
           <li class="list-group-item">GitHub: <a href="${eng.github}" target="_blank">${eng.gitHub}</a></li>
@@ -87,12 +87,12 @@ function generateEngineerDiv(eng) {
 
 function generateInternDiv(intern) {
   return `
-  <div class="card m-3 p-0 bg-light border border-secondary" style="width: 18rem;">
-      <div class="card-body bg-success text-white">
+  <div class="card m-3 p-0 bg-light border" style="width: 18rem;">
+      <div class="card-body bg-secondary text-white">
           <h5 class="card-title">${intern.empName}</h5>
           <p class="card-text">Intern</p>
       </div>
-      <ul class="list-group list-group-flush m-3 border border-secondary">
+      <ul class="list-group m-3">
           <li class="list-group-item">ID: ${intern.id}</li>
           <li class="list-group-item">Email: <a href="mailto:${intern.empEmail}">${intern.empEmail}</a></li>
           <li class="list-group-item">School: ${intern.school}</li>
